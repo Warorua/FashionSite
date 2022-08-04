45864eb7779fdb8a5f1743bcc46aa3337144a6f4
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2022-08-04 20:09:02 +0300

    SMTP updates

--- fashionsite/send.php
+++ fashionsite/send.php
@@ -10,7 +10,55 @@ $comment="Sent Email".$Message;
     mail($Email,$Message,$comment);
     echo "Mail Sent";
 
+//MY ADDED SMTP SECTION
 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+				
+				
+					$message = "
+					   This is the message that will be sent by the smtp. You can edit it however you want. 
+                       You can add HTML here for styled message. However, the HTML should use Inline CSS ONLY
+					";
+
+					//Load phpmailer
+		    		require 'vendor/autoload.php';
+
+		    		$mail = new PHPMailer(true);                             
+				
+				        //Server settings
+						
+				        $mail->isSMTP();                                     
+				        $mail->Host = gethostbyname('mail.tsavo.store'); //------This is the server host                  
+				        $mail->SMTPAuth = true;                               
+				        $mail->Username = 'noreply@tsavo.store'; //---------the email sending   
+				        $mail->Password = '@C3yaEsrjusrusdrXFtUt'; //---------the password of the email sending                      
+				        $mail->SMTPOptions = array(
+				            'ssl' => array(
+				            'verify_peer' => false,
+				            'verify_peer_name' => false,
+				            'allow_self_signed' => true
+				            )
+				        );                         
+				        $mail->SMTPSecure = 'tls';                           
+				        $mail->Port = 587;  //---------the port from which smtp will use                                    
+
+				        $mail->setFrom('noreply@tsavo.store'); //---------useful but not necessary
+				        
+				        //Recipients
+				        $mail->addAddress($email);  //---------the email receiving             
+				        $mail->addReplyTo('waroruaalex@tsavo.store'); //---------the email receiving a copy
+				       
+				        //Content
+				        $mail->isHTML(true); //--------- =TRUE if you have HTML in you message ELSE FALSE if you dont have                                  
+				        $mail->Subject = 'TSAVO 254'; //---------the subject of the email     
+				        $mail->Body    = $message; //---------the message body of the email  
+
+				        $mail->send();
+
+	
+
+//END OF MY CODE
 
 
 
